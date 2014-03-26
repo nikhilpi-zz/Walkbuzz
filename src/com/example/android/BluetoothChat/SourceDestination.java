5b03098395c8abf5b5d5248975ae4a3865cd87a7
Shikhar Mohan <shikharmohan2016@u.northwestern.edu>
2014-03-25 21:13:05 -0500

    added google play services

--- src/com/example/android/BluetoothChat/SourceDestination.java
+++ src/com/example/android/BluetoothChat/SourceDestination.java
@@ -1,12 +1,18 @@
 package com.example.android.BluetoothChat;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,33 +20,43 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 /**
  * Created by home on 3/24/14.
  */
 public class SourceDestination extends Activity {
+    //Debugging
+    private TextView locationdata;
+    private static final String TAG = "SourceDestination";
+    Location currLoc; // current location
 
-       @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.source_dest);
+
+        TextView locationdata = (TextView) findViewById(R.id.location);
 
 
         Button btn = (Button) findViewById(R.id.bluebutton);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SourceDestination.this,BluetoothChat.class);
+                Intent intent = new Intent(SourceDestination.this, BluetoothChat.class);
                 startActivity(intent);
             }
         });
 
-           LocationManager manager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-           LocationListener listen = new MyLocationListener();
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        LocationListener listen = new MyLocationListener();
+        currLoc = new Location(LocationManager.GPS_PROVIDER);
+        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, listen);
 
-           manager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 0, 0, listen);
 
-
-       }
+    }
 
 
     /* Class My Location Listener */
@@ -49,6 +65,7 @@ public class SourceDestination extends Activity {
 
     {
 
+        @TargetApi(Build.VERSION_CODES.GINGERBREAD)
         @Override
 
         public void onLocationChanged(Location loc)
@@ -62,8 +79,9 @@ public class SourceDestination extends Activity {
             double lon = loc.getLongitude();
 
             String txt = "" + strLatitude + "\n " + strLongitude;
-            TextView locationdata = (TextView) findViewById(R.id.location);
             locationdata.setText(txt);
+
+            currLoc = loc;
 
         }
 
@@ -74,7 +92,7 @@ public class SourceDestination extends Activity {
 
         {
 
-            Toast.makeText( getApplicationContext(),"GPS Disabled", Toast.LENGTH_SHORT ).show();
+            Toast.makeText(getApplicationContext(), "GPS Disabled", Toast.LENGTH_SHORT).show();
 
         }
 
@@ -85,7 +103,7 @@ public class SourceDestination extends Activity {
 
         {
 
-            Toast.makeText(getApplicationContext(),"GPS Enabled", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "GPS Enabled", Toast.LENGTH_SHORT).show();
 
         }
 
@@ -100,7 +118,6 @@ public class SourceDestination extends Activity {
         }
 
     }/* End of Class MyLocationListener */
-
 
 }
 
